3287cac34f77961b738704164b1ef99db8f76385
Zack Fisch <zack@zfisch.com>
2014-06-23 18:12:32 -0400

    Add contact page, and 'red' Rspec test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -35,13 +35,20 @@ describe "Static pages" do
 		it "should have the content 'About Us' " do
 			visit '/static_pages/about'
 			expect(page).to have_content('About Us')
+		end
 
 		it "should have the right title" do
 			visit '/static_pages/home'
 			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
 		end
 
+	end
+	
+	describe "Contact page" do
 
+		it "should have the content 'Contact' " do
+			visit 'static_pages/contact'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
 		end
 	end
 end
